20655ee348e566fd22956470d19c0bbee23bafc5
Edwin <edwin@am.is>
2018-09-11 23:01:25 +0800

    ethclient: add SubscribePendingTransactions/SubscribeQueuedTransactions API

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -313,6 +313,14 @@ func (ec *Client) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header)
 	return ec.c.EthSubscribe(ctx, ch, "newHeads")
 }
 
+func (ec *Client) SubscribePendingTransactions(ctx context.Context, ch chan<- common.Hash) (ethereum.Subscription, error) {
+	return ec.c.EthSubscribe(ctx, ch, "newPendingTransactions")
+}
+
+func (ec *Client) SubscribeQueuedTransactions(ctx context.Context, ch chan<- common.Hash) (ethereum.Subscription, error) {
+	return ec.c.EthSubscribe(ctx, ch, "newQueuedTransactions")
+}
+
 // State Access
 
 // NetworkID returns the network ID (also known as the chain ID) for this chain.
